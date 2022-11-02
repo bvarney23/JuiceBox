050778ab44d463bcbb783ae67b310af00d42ae77
Brandon Varney <b.varney03@gmail.com>
2022-11-02 17:26:27 -0400

    cleanup

--- db/index.js
+++ db/index.js
@@ -2,30 +2,31 @@ const { Client } = require('pg');
 
 const client = new Client('postgres://localhost:5432/juicebox-dev');
 
-
-async function getAllUsers() {
-    const { rows } = await client.query(
-        `SELECT id, username
-        from users;
-        `);
-
-    return rows
-}
-
-async function createUser({ username, password }) {
+async function createUser({ username, password, name, location }) {
     try {
       const { rows } = await client.query(`
-        INSERT INTO users(username, password)
-        VALUES ($1, $2)
+        INSERT INTO users(username, password, name, location)
+        VALUES ($1, $2, $3, $4)
         ON CONFLICT (username) DO NOTHING
         RETURNING *;
-      `, [username, password]);
+      `, [username, password, name, location]);
   
       return rows;
     } catch (error) {
       throw error;
     }
   }
+
+async function getAllUsers() {
+    const { rows } = await client.query(
+        `SELECT id, username, name, location, active
+        from users;
+        `);
+
+    return rows
+}
+
+
 
 module.exports = {
     client,
